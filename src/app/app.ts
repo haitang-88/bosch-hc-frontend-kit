9945f1f223d6e9f5e1d8547c8812eca03d7d5072
Hai Tang Chau Viet <aha4hc@bosch.com>
2026-08-03 12:44:23 +0700

    added theme changer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   title = 'Bosch HC Frontend Kit';
+  theme = 'light-mode';
 }
