9a817a4edeb4513da84fd26de396e15335451eef
Hai Tang Chau Viet <aha4hc@bosch.com>
2026-08-04 22:39:21 +0700

    completed dashboard UI

--- src/app/pages/project-dashboard/project-dashboard.ts
+++ src/app/pages/project-dashboard/project-dashboard.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 
 @Component({
   selector: 'app-project-dashboard',
   imports: [CommonModule],
   templateUrl: './project-dashboard.html',
   styleUrl: './project-dashboard.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ProjectDashboard {
-  isLeftPanelOpen = false;
+export class ProjectDashboard implements OnInit {
+  isLeftPanelOpen = true;
   isRightPanelOpen = false;
+   public activeTabId = signal<string>('tab-1');
 
   toggleLeftPanel() {
     this.isLeftPanelOpen = !this.isLeftPanelOpen;
@@ -17,5 +19,37 @@ export class ProjectDashboard {
 
   toggleRightPanel() {
     this.isRightPanelOpen = !this.isRightPanelOpen;
+  }
+  ngOnInit(): void {
+    const contents = Array.from(
+      document.querySelectorAll(
+        '.frontend-kit-example_content',
+      ),
+    ) as HTMLDivElement[];
+    const tabs = document.querySelector('.a-tab-navigation');
+
+    if (tabs) {
+      //const component = tabs as unknown as EventTarget;
+
+      tabs.addEventListener('selected', (id) => {
+        contents.forEach((content) => {
+          if (content.dataset?.['frokContentIdentifier'] === id?.toString()) {
+            /* eslint-disable-next-line no-param-reassign */
+            content.style.display = 'block';
+          } else {
+            /* eslint-disable-next-line no-param-reassign */
+            content.style.display = 'none';
+          }
+        });
+      });
+    }
+  }
+
+  public onTabSelected(event: Event): void {
+    // Web Components emit standard CustomEvents. 
+    // The selected tab ID is usually found in event.detail
+    const customEvent = event as CustomEvent;
+    console.log('Tab selected:', customEvent.detail);
+    //this.activeTabId.set(customEvent.detail);
   }
 }
